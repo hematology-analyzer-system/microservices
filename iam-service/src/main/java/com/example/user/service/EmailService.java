2ad93e51bdedcdb15d641a27d8c155b7a383fe4b
LancerHihi <166286546+LancerHihi@users.noreply.github.com>
2025-08-06 16:48:18 +0700

    fix(smtp): change domain to send email, using MailTrap

--- iam-service/src/main/java/com/example/user/service/EmailService.java
+++ iam-service/src/main/java/com/example/user/service/EmailService.java
@@ -1,8 +1,10 @@
 package com.example.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +13,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("smtp.verify.token@gmail.com"); // Configure this in application.properties
+        message.setFrom("no-reply@khoa.email"); // Configure this in application.properties
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
